f33761ed9e63dd3ab9adfbdacd01ee9f73502531
Manh Cuong <cuongpigerr@gmail.com>
2021-06-08 23:41:33 +0700

    .

---
+++ app/src/modules/ChatUI.java
@@ -0,0 +1,13 @@
+package modules;
+
+import javax.swing.*;
+
+public class ChatUI {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JButton button1;
+    private JTextArea textArea2;
+    private JTextField textField1;
+    private JButton button2;
+
+}
