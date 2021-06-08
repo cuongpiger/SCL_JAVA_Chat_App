a7a72fdf11ec908fdb866d07e94d8506f19718a8
Manh Cuong <cuongpigerr@gmail.com>
2021-06-08 23:30:36 +0700

    .

---
+++ app/src/modules/LoginUI.java
@@ -0,0 +1,10 @@
+package modules;
+
+import javax.swing.*;
+
+public class LoginUI {
+    private JPanel vPanel;
+    private JTextField vAcc;
+    private JTextField vPass;
+    private JButton vButton;
+}
