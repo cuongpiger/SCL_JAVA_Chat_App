19d9823bd3655d21e94eabcc2b1b944135f95d0d
Manh Cuong <cuongpigerr@gmail.com>
2021-06-10 11:47:26 +0700

    .

--- app/src/modules/ChatBoxUI.java
+++ app/src/modules/ChatBoxUI.java
@@ -7,6 +7,7 @@ import java.awt.event.WindowEvent;
 
 public class ChatBoxUI extends JFrame implements ActionListener {
     private JPanel panel1;
+    private String iFriend = null;
     public JTextArea vContent;
     public JTextArea vText;
     public JButton vSend;
@@ -18,6 +19,7 @@ public class ChatBoxUI extends JFrame implements ActionListener {
 
     public ChatBoxUI(String pTitle) {
         super(pTitle);
+        iFriend = pTitle;
         setContentPane(panel1);
         ClientController.sendPackage("KEEP-CONNECT", null);
         vSend.addActionListener(this);
@@ -26,7 +28,10 @@ public class ChatBoxUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent pae) {
         if (pae.getSource() == vSend) {
             if (vText.getText().isEmpty() == false) {
-
+                Message mess = new Message(LoginUI.iUser.getiAccount(), iFriend, vText.getText());
+                vContent.append(LoginUI.iUser.getiAccount() + ": " + vText.getText() + "\n");
+                vText.setText("");
+                ClientController.sendPackage("SEND-MESSAGE", mess);
             }
         }
     }
